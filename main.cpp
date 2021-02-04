25c8e066dfb6b3f49216bce8fe753d3ce8ee82bb
IPlayZed <pauljouser@gmail.com>
2021-02-04 22:17:54 +0100

    Created window and glad init check. TODO: add glad source as cmake project(, or compile it?)
    Signed-off-by: IPlayZed <pauljouser@gmail.com>

--- main.cpp
+++ main.cpp
@@ -9,10 +9,25 @@ void error_callback(int err, const char *desc) {
 }
 
 int main() {
+    //TODO: restructure project so glad can be added as a CMake subdir (probably clone source?)
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cerr << "GLAD failed to initialize\n";
+        return -1;
+    }
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *glfWwindow = glfwCreateWindow(960, 540, "Probe", NULL, NULL);
+    if (glfWwindow == NULL) {
+        std::cerr << "GLFW window failed to initialize!\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(glfWwindow);
 
     return 0;
 }
