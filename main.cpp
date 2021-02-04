e0ac70f6426937e27bf6c728a9e66bad9b0950de
IPlayZed <pauljouser@gmail.com>
2021-02-04 21:58:37 +0100

    Added glad deps and main skeleton for executable.
    Signed-off-by: IPlayZed <pauljouser@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
+#define GLFW_INCLUDE_NONE
+
 #include <iostream>
+#include "deps/src/glad/include/glad/glad.h"
+#include "deps/src/glfw/glfw-3.3.2/include/GLFW/glfw3.h"
+
+void error_callback(int err, const char *desc) {
+    fprintf(stderr, "Error: %s\n", desc);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     return 0;
 }
